a657720d376ab13d07924898bde88c1f018c85fa
s-aajain <tomatochamp@hotmail.com>
2021-07-28 17:54:45 -0700

    trained accelX

--- alg/gain/runner.py
+++ alg/gain/runner.py
@@ -6,10 +6,10 @@ import numpy as np
 import matplotlib.pyplot as plt 
 #import pyplot as plt
 
-file_to_rename = "accel_x_imputed_train.csv"
+file_to_rename = "accel_y_imputed_train.csv"
 print("-------------------------------------- stretch missing third layer 1 --------------------------------")
-os.system("python gain.py -i accel_x_train.csv -o accel_x_imputed_train.csv --T 2 --f 1 --s 1 --t 1 --testfile accel_x_test.csv")
-os.rename(file_to_rename, "accel_x_imputed_train_d3_1.csv")
+os.system("python gain.py -i accel_y_train.csv -o accel_y_imputed_train.csv --T 2 --f 1 --s 1 --t 1 --testfile accel_y_test.csv")
+os.rename(file_to_rename, "accel_y_imputed_train_d3_1.csv")
 print("-------------------------------------- stretch missing third layer 1 --------------------------------")
 
 
@@ -17,8 +17,8 @@ print()
 
 
 print("-------------------------------------- stretch missing third layer 2 --------------------------------")
-os.system("python gain.py -i accel_x_train.csv -o accel_x_imputed_train.csv --T 2 --f 1 --s 1 --t 2 --testfile accel_x_test.csv")
-os.rename(file_to_rename, "accel_x_imputed_train_d3_2.csv")
+os.system("python gain.py -i accel_y_train.csv -o accel_y_imputed_train.csv --T 2 --f 1 --s 1 --t 2 --testfile accel_y_test.csv")
+os.rename(file_to_rename, "accel_y_imputed_train_d3_2.csv")
 
 print("-------------------------------------- stretch missing third layer 2 --------------------------------")
 
@@ -27,8 +27,8 @@ print()
 
 
 print("-------------------------------------- stretch missing third layer 4 --------------------------------")
-os.system("python gain.py -i accel_x_train.csv -o accel_x_imputed_train.csv --T 2 --f 1 --s 1 --t 4 --testfile accel_x_test.csv")
-os.rename(file_to_rename, "accel_x_imputed_train_d3_4.csv")
+os.system("python gain.py -i accel_y_train.csv -o accel_y_imputed_train.csv --T 2 --f 1 --s 1 --t 4 --testfile accel_y_test.csv")
+os.rename(file_to_rename, "accel_y_imputed_train_d3_4.csv")
 
 print("-------------------------------------- stretch missing third layer 4 --------------------------------")
 
@@ -37,7 +37,7 @@ print()
 
 
 print("-------------------------------------- stretch missing third layer 8 --------------------------------")
-os.system("python gain.py -i accel_x_train.csv -o accel_x_imputed_train.csv --T 2 --f 1 --s 1 --t 8 --testfile accel_x_test.csv")
-os.rename(file_to_rename, "accel_x_imputed_train_d3_8.csv")
+os.system("python gain.py -i accel_y_train.csv -o accel_y_imputed_train.csv --T 2 --f 1 --s 1 --t 8 --testfile accel_y_test.csv")
+os.rename(file_to_rename, "accel_y_imputed_train_d3_8.csv")
 print("-------------------------------------- stretch missing third layer 8 --------------------------------")
 print()
